3b9304a569be8136a3985b63fe6fd65bc219db68
Bartek <depciuch322@gmail.com>
2024-05-22 21:47:29 +0200

    Added alias configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@variables': path.resolve(__dirname, 'src/assets/styles/variables'),
+      '@globals': path.resolve(__dirname, 'src/assets/styles/globals'),
+      '@images': path.resolve(__dirname, 'src/assets/images')
+    }
+  },
+  plugins: [react()]
+});
