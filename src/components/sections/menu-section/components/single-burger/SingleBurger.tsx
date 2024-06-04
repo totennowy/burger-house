fa23337fc69324ef981ec9bfd98fc75bf38c7a8e
Bartek <depciuch322@gmail.com>
2024-06-04 14:09:11 +0200

    Changed button in SingleBurger component

--- src/components/sections/menu-section/components/single-burger/SingleBurger.tsx
+++ src/components/sections/menu-section/components/single-burger/SingleBurger.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import styles from './styles/singleBurger.module.scss';
 import { ModelSingleBurger } from './types/modelSingleBurger';
+import ButtonRed from '../../../../commons/button-red/ButtonRed';
 
 const SingleBurger: FC<ModelSingleBurger> = ({ name, description, image }) => {
   return (
@@ -8,9 +9,7 @@ const SingleBurger: FC<ModelSingleBurger> = ({ name, description, image }) => {
       <img src={image} alt={name} className={styles.burger_img} />
       <p className={styles.burger_name}>{name}</p>
       <p className={styles.burger_description}>{description}</p>
-      <button className={styles.burger_button}>
-        <span>ORDER NOW</span>
-      </button>
+      <ButtonRed text="ORDER NOW" type="button" />
     </div>
   );
 };
